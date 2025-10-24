a373bc88586d9ab28b7dea39ba264e0b18df2aa6
daraivanova <kudiyarova.1975@gmail.com>
2025-10-24 03:53:44 +0300

    Создан пайп для конвертации строки в масив

---
+++ src/app/helpers/pipes/convert-array.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convertArray',
+})
+export class ConvertArrayPipe implements PipeTransform {
+  transform(value: string): string[] | null {
+    if (!value) return null;
+    return value.split(',');
+  }
+}
